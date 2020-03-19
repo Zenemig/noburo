4b08945f13f7c89e7727e2fc8d1939fbc216c01c
--global <hola@zenemig.net>
2020-03-19 12:10:26 -0300

    feat(home): add content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <p>
+      <strong>NO BÜRO</strong> es un estudio independiente de diseño gráfico
+      ubicado en Santiago, Chile.
+    </p>
+    <p>
+      Trabajamos en el área de la cultura, las artes y diferentes tipos de
+      clientes, enfocados en la práctica de publicaciones impresas y digitales,
+      identidad y dirección de arte.
+    </p>
+    <p>
+      Definimos al diseño como un proceso visual basado en el conocimiento
+      técnico y principios estéticos, los cuales responden a una necesidad
+      específica comercial o artística.
+    </p>
+    <p>
+      Generalmente toma un tiempo para crear y conducir un proceso de
+      investigación, análisis y creación de una solución visual contundente que
+      dé satisfacción a las necesidades de cada cliente.
+    </p>
+    <p>
+      Buscamos que nuestro trabajo esté dirigido en resolver los problemas de
+      comunicación, encontrando la manera más efectiva para transmitir la
+      información mediante un concepto visual, elección tipográfica o
+      diagramación de una página.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home"
-  // components: {
-  //   HelloWorld
-  // }
 };
 </script>
